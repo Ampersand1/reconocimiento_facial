be5c5812178b0504d68aa8371e9bbd12fd1497b1
Ampersand1 <babyv1905@gmail.com>
2024-06-08 04:32:20 -0500

    Conectar back-front
    cambio en el js de agregar

--- static/agregar.js
+++ static/agregar.js
@@ -1,40 +1,39 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const agregar = document.getElementById('agregar');
-    agregar.addEventListener('click', async (event) => {
-        event.preventDefault();  // Previene que el formulario se envíe de la forma tradicional
+document.addEventListener("DOMContentLoaded", function() {
+    const mostrarAgregarBtn = document.getElementById("mostrarAgregar");
+    const formContainer = document.getElementById("formContainer");
+    const userForm = document.getElementById("userForm");
+    const btnAgregarUsuario = document.getElementById("btnAgregarUsuario");
 
-        const name = document.getElementById('nombre').value;
-        const photos = document.getElementById('files').files;
-
-        const formData = new FormData();
-        formData.append('nombre', name);
-        for (const photo of photos) {
-            formData.append('files', photo);
-        }
-
-        try {
-            const response = await fetch('http://localhost:8000/agregar-usuario/', {
-                method: 'POST',
-                body: formData
-            });
-
-            if (!response.ok) {
-                throw new Error(`Error: ${response.statusText}`);
-            }
-
-            // Manejar la respuesta exitosa
-            showMessage('Usuario agregado exitosamente', 'success');
-        } catch (error) {
-            showMessage(`Error: ${error.message}`, 'error');
-        }
+    mostrarAgregarBtn.addEventListener("click", function() {
+        mostrarAgregarBtn.style.display = "none";
+        formContainer.style.display = "flex";
     });
 
-    function showMessage(message, type) {
-        const messageParagraph = document.createElement('p');
-        messageParagraph.textContent = message;
-        messageParagraph.classList.add(type);
-        document.body.appendChild(messageParagraph);
-    }
+    btnAgregarUsuario.addEventListener("click", function() {
+        userForm.style.display = "block";
+        btnAgregarUsuario.style.display = "none";
+    });
 
-    
+    userForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+        const formData = new FormData(userForm);
+        
+        fetch("http://localhost:8000/agregar-usuario/", {
+            method: "POST",
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            alert("Usuario agregado exitosamente");
+            userForm.reset();
+            userForm.style.display = "none";
+            btnAgregarUsuario.style.display = "block";
+        })
+        .catch(error => {
+            console.error(error);
+            alert("Error al agregar usuario");
+        });
+    });
 });
+

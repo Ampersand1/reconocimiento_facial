9802c229cc51002cc2e107dfdc4b8a9e8672a923
Ampersand1 <124942060+Ampersand1@users.noreply.github.com>
2024-06-08 07:39:27 -0500

    Update agregar.js

--- static/agregar.js
+++ static/agregar.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const agregar = document.getElementById('agregar');
+    const agregar = document.getElementById('btnAgregar');
+    const btnSubirFoto = document.getElementById("btnSubirFoto");
+    const fileInput = document.getElementById("files");
+
+    if (btnSubirFoto && fileInput) {
+        btnSubirFoto.addEventListener("click", function() {
+            fileInput.click();
+        });
+
+        fileInput.addEventListener("change", function() {
+            // Aquí puedes agregar lógica adicional para manejar la selección de archivos si es necesario
+        });
+    } else {
+        console.error("Uno o más elementos no fueron encontrados en el DOM.");
+    }
     agregar.addEventListener('click', async (event) => {
         event.preventDefault();  // Previene que el formulario se envíe de la forma tradicional
 
@@ -28,13 +42,11 @@ document.addEventListener('DOMContentLoaded', () => {
             showMessage(`Error: ${error.message}`, 'error');
         }
     });
-
     function showMessage(message, type) {
         const messageParagraph = document.createElement('p');
         messageParagraph.textContent = message;
         messageParagraph.classList.add(type);
         document.body.appendChild(messageParagraph);
     }
-
-    
 });
+
